2f88377a734bf82c5ebc301fb34aaf862fd38983
deLoretoMelo <meloloretomarcelo@gmail.com>
2024-05-15 20:53:44 -0300

    reescrevendo o About

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -20,7 +20,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC app from C# course";
+            ViewData["Email"] = "melo@gmail.com";
 
             return View();
         }
